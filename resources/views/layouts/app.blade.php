ff27a05c926c0363ccc7cb48d1c7a0ff184098e2
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-10-08 01:14:57 +0200

    fixed issues, ready to go (?)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,10 +67,10 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('dashboard') }}">{{__('Dashboard')}}</a>
-                                <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                                <a class="dropdown-item" href="{{ url('admin/admin') }}">{{__('Dashboard')}}</a>
+                                <a class="dropdown-item" href="{{ url('admin/profile') }}">{{__('Profile')}}</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                    document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
 
